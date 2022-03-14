75deb6be85dc9f3ce6c11bd3b57bc69d8f5decb0
Pravesh Singh <pravesh.singh@citymall.live>
2022-03-14 09:25:49 +0530

    Added use of Animated.spring and getLayout

--- src/screens/MovingBoll/movingBoll.tsx
+++ src/screens/MovingBoll/movingBoll.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { StyleSheet, Text, View, Animated } from 'react-native'
 
 type Props = {}
 
 const MovingBoll = (props: Props) => {
+
+  let position = new Animated.ValueXY({ x: 0, y: 0 });
+
+  useEffect(() => {
+    setTimeout(() => {
+      Animated.spring(position, {
+        toValue: { x: 200, y: 500 },
+        useNativeDriver: false
+      }).start()
+    }, 1000);
+  }, [])
+
+
   return (
-    <View style={styles.container}>
-      <Text>movingBoll</Text>
-    </View>
+    <Animated.View style={position.getLayout()}>
+      <View style={styles.ball} />
+    </Animated.View>
   )
 }
 
@@ -16,5 +29,12 @@ export default MovingBoll;
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  ball: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    borderColor: 'black',
+    borderWidth: 30
   }
 })
